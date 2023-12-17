7527658c5e3f03a202d41547015722032cbbb23b
Pixomia <20332831+Pixomia@users.noreply.github.com>
2023-12-17 12:22:58 +0100

    first init test spring boot and github action

---
+++ src/main/java/com/github/pixomia/gittertester/GittertesterApplication.java
@@ -0,0 +1,13 @@
+package com.github.pixomia.gittertester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittertesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittertesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pixomia/gittertester/GittertesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pixomia.gittertester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittertesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
