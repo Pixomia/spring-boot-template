5ba1de6b8bbeeda424b6a3d8fe9a159da1b0bbea
Pixomia <20332831+Pixomia@users.noreply.github.com>
2023-12-17 12:46:34 +0100

    first init of test app with github actions and external and pixomia dep

---
+++ src/main/java/com/github/pixomia/gittertester/BackgroundTaskker.java
@@ -0,0 +1,14 @@
+package com.github.pixomia.gittertester;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.github.pixomia.util.PixomiaIdUtils;
+
+@Service
+public class BackgroundTaskker {
+    @Scheduled(fixedDelay = 1000)
+    public void backGroundTassker() {
+	System.out.println(" Bingo Bongo " + PixomiaIdUtils.genHumandReadableId() + "");
+    }
+}
--- src/main/java/com/github/pixomia/gittertester/GittertesterApplication.java
+++ src/main/java/com/github/pixomia/gittertester/GittertesterApplication.java
@@ -2,12 +2,16 @@ package com.github.pixomia.gittertester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@Configuration
+@EnableScheduling
 public class GittertesterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GittertesterApplication.class, args);
-	}
+    public static void main(final String[] args) {
+	SpringApplication.run(GittertesterApplication.class, args);
+    }
 
 }
